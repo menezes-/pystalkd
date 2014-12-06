e254c662ef47da428a8fcd3cb904a15ab29cb710
Gabriel <gabrielmenezesvi@gmail.com>
2014-12-06 15:34:02 -0200

    add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pystalkd',
+    version='1.0',
+    packages=['pystalkd'],
+    url='https://github.com/menezes-/pystalkd',
+    download_url='https://github.com/menezes-/pystalkd/archive/master.zip',
+    keywords=['beanstalkd', 'python3', 'bindings'],
+    license='Apache-2.0',
+    author='Gabriel',
+    author_email='gabrielmenezesvi@gmail.com',
+    description='Beanstalkd bindings for python3'
+)
